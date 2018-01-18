0760f78e50f09abfefb4ac2fce08059d3dc74bfe
Vladimir Ivanin <ivaninvv@mail.ru>
2018-01-18 14:28:46 +0300

    Не подтягивать не существующие товары

--- app/getProductList.js
+++ app/getProductList.js
@@ -9,24 +9,24 @@ import convertProperties from './convertProperties.js';
 export function getProductList (productList) {
   var self = this;
   return $.when(_getProducts())
-
+  
   function _getProducts() {
     var dfd = jQuery.Deferred();
     var isArray = (Object.prototype.toString.call( productList ) == '[object Array]');
     var resultList = {};
-
+    
     if (!isArray) {
       self.logger('Список id, не является массивом', productList);
       dfd.reject(resultList);
     }
-
+    
     if (isArray && productList.length == 0) {
       self.logger('Список id пуст', productList);
       dfd.reject(resultList);
     }
-
+    
     if (isArray && productList.length > 0) {
-
+      
       // товары с помощью common js v2
       if (typeof Products == 'object' && Products.getList) {
         Products.getList(productList)
@@ -47,95 +47,23 @@ export function getProductList (productList) {
           dfd.reject( {} );
         });
       }else{
-        // получаем товары сохраненные в локальном хранилище
-        getLocalProduct(self, system.keyProducts).done(function (_productsList) {
-          var missingList = [];
-          $.each(productList, function(index, el) {
-            if (!_productsList[el]) {
-              missingList.push( el )
-            }
+        getApiProduct(self, productList).done(function (_productsObject) {
+          self.logger('Товары из стандартного апи: ', _productsObject);
+          $.each(_productsObject, function(index, _product) {
+            convertProperties(_product);
           });
-
-          // если не достает каких то товаров, получить их из апи
-          if (missingList.length > 0) {
-            getApiProduct(self, missingList).done(function (_productsObject) {
-              self.logger('Товары из стандартного апи: ', _productsObject);
-              $.each(_productsObject, function(index, _product) {
-                convertProperties(_product);
-              });
-              dfd.resolve( $.extend(true, {}, _productsList, _productsObject) );
-            })
-            .fail(function (onFail) {
-              dfd.reject( {} );
-            });
-          }else{
-            $.each(_productsList, function(index, _product) {
-              convertProperties(_product);
-            });
-            dfd.resolve( _productsList );
-          }
-        }).fail(function () {
-          // если в хранилище нет товаров, берем из стандартного апи
-          getApiProduct(self, productList).done(function (_productsObject) {
-            var sizeProductsObject = Object.keys(_productsObject).length;
-            if (sizeProductsObject > 0) {
-              setLocalProduct(self, system.keyProducts, _productsObject)
-              self.logger('Товары из стандартного апи: ', _productsObject);
-              $.each(_productsObject, function(index, _product) {
-                convertProperties(_product);
-              });
-              dfd.resolve( convertProductList(_productsObject) );
-            }else{
-              dfd.reject( {} );
-            }
-          })
-          .fail(function (onFail) {
-            dfd.reject( {} );
-          });
+          dfd.resolve( $.extend(true, {}, _productsList, _productsObject) );
+        })
+        .fail(function (onFail) {
+          dfd.reject( {} );
         });
       }
-
+      
     }
-
+    
     return dfd.promise();
   }
 }
-
-// Сохранить товары в localforage
-const setLocalProduct = function (self, keyProducts, products) {
-  localforage.setItem(keyProducts, products, function(err) {
-    if (err) {
-      self.logger('Не удалось сохранить товары в localforage');
-    }else{
-      self.logger('Данные сохранены в localforage');
-    }
-  });
-}
-
-// Получить данные из хранилища
-// localData - объект
-const getLocalProduct = function (self, keyProducts) {
-  return $.when(_getLocalData())
-
-  function _getLocalData() {
-    var dfd = jQuery.Deferred();
-
-    localforage.getItem(keyProducts, function(err, localData) {
-      if (localData) {
-        self.logger('Данные получены из хранилища', localData);
-
-        dfd.resolve( convertProductList(localData) );
-      }else{
-        self.logger('Хранилище пусто');
-
-        dfd.reject('Хранилище пусто');
-      }
-    });
-
-    return dfd.promise();
-  }
-};
-
 // получить товары из апи
 // _productsObject - объект
 const getApiProduct = function (self, _products) {
